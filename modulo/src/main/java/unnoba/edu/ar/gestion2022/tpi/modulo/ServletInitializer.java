e4dffea6a1b10e1f7a7fbe3af2f69ee04535381b
Facundo Argento <facuargento@hotmail.com>
2022-06-02 17:48:15 -0300

    Primer commit. Configuraciones basicas de spring y de postgres agregadas.

---
+++ modulo/src/main/java/unnoba/edu/ar/gestion2022/tpi/modulo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package unnoba.edu.ar.gestion2022.tpi.modulo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ModuloApplication.class);
+	}
+
+}
---
+++ modulo/src/test/java/unnoba/edu/ar/gestion2022/tpi/modulo/ModuloApplicationTests.java
@@ -0,0 +1,13 @@
+package unnoba.edu.ar.gestion2022.tpi.modulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
